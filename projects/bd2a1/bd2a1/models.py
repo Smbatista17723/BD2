2931b2eba535b67f8e6bd4450ef18a6a8d998265
Smbatista17723 <116801995+Smbatista17723@users.noreply.github.com>
2022-11-29 19:26:57 +0000

    Delete projects/bd2a1 directory

--- projects/bd2a1/bd2a1/models.py
+++
@@ -1,8 +0,0 @@
-
-from django.db import models
-
-class UtiModel(models.Model):
-    desig=models.CharField(max_length = 100)
-    passwd=models.CharField(max_length = 100)
-    class Meta:
-        db_table="utilizadores"
