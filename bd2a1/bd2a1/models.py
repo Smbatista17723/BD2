86162b6917d800f0e710863b3f9f3ac405ecbf48
Smbatista17723 <116801995+Smbatista17723@users.noreply.github.com>
2023-01-12 18:03:43 +0000

    Delete bd2a1 directory

--- bd2a1/bd2a1/models.py
+++
@@ -1,8 +0,0 @@
-
-from django.db import models
-
-class Enc(models.Model):
-    e_id = models.IntegerField()
-    data = models.DateField()
-    email = models.CharField(max_length=100)
-    preço = models.FloatField()
